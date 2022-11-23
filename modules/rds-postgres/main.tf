3acf68ec76c2b342d96c612c886cf1a66a5b376c
cirrostech <56857967+cirrostech@users.noreply.github.com>
2022-11-23 23:54:59 +0800

    Add files via upload

---
+++ modules/rds-postgres/main.tf
@@ -0,0 +1,125 @@
+locals {
+  # create_password_secret    = var.password == null && var.create_secretmanager_secret ? true : false
+  # create_password_parameter = var.password == null && var.create_ssm_secret ? true : false
+  final_snapshot_identifier = var.final_snapshot_identifier == null ? "${var.name}-final-snapshot" : var.final_snapshot_identifier
+  monitoring_role_arn       = try(aws_iam_role.this[0].arn, var.monitoring_role_arn)
+  parameter_group_name      = length(var.parameters) > 0 ? aws_db_parameter_group.this[0].name : null
+  sg_name_prefix            = "${var.name}-access"
+  # ssm_path                  = coalesce(var.ssm_path, "/db/${var.name}/${var.username}-password")
+
+  db_tags = merge(
+    var.tags,
+    {
+      "Name" = "${var.name}-postgres"
+    },
+  )
+
+  sg_tags = merge(
+    var.tags
+  )
+}
+
+resource "aws_db_parameter_group" "this" {
+  count = length(var.parameters) > 0 ? 1 : 0
+
+  name_prefix = "${var.name}-param"
+
+  family = coalesce(
+    var.parameter_group_family,
+    "postgres${replace(var.engine_version, "/\\.\\d+/", "")}" # strips the minor and patch digits from the version
+  )
+
+  dynamic "parameter" {
+    iterator = each
+    for_each = var.parameters
+
+    content {
+      name  = each.value.name
+      value = each.value.value
+    }
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["monitoring.rds.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  count              = var.monitoring_interval > 0 && var.monitoring_role_arn == null ? 1 : 0
+  name_prefix        = var.name
+  assume_role_policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_security_group" "this" {
+  name_prefix = local.sg_name_prefix
+
+  description = "RDS instance SG"
+  tags        = local.sg_tags
+  vpc_id      = var.vpc_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_groups" {
+  count = length(var.allowed_security_groups)
+
+  description              = "Permit RDS access (${var.allowed_security_groups[count.index]})"
+  from_port                = var.port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.this.id
+  source_security_group_id = var.allowed_security_groups[count.index]
+  to_port                  = var.port
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "allow_ipv4_cidrs" {
+  count = length(var.allowed_cidr_blocks) > 0 ? 1 : 0
+
+  cidr_blocks       = var.allowed_cidr_blocks
+  description       = "Permit access to CIDRs"
+  from_port         = var.port
+  protocol          = "tcp"
+  security_group_id = aws_security_group.this.id
+  to_port           = var.port
+  type              = "ingress"
+}
+
+resource "aws_db_instance" "this" {
+  identifier        = try(substr(var.identifier, 0, 63), null)
+  identifier_prefix = try(substr(var.identifier_prefix, 0, 36), null)
+
+  allocated_storage                   = var.storage
+  backup_retention_period             = var.backup_retention_period
+  copy_tags_to_snapshot               = true
+  db_subnet_group_name                = var.subnet_group_name
+  deletion_protection                 = var.enable_deletion_protection
+  enabled_cloudwatch_logs_exports     = var.cloudwatch_log_exports
+  engine                              = "postgres"
+  engine_version                      = var.engine_version
+  final_snapshot_identifier           = local.final_snapshot_identifier
+  iam_database_authentication_enabled = var.iam_database_authentication_enabled
+  instance_class                      = var.instance_class
+  monitoring_interval                 = var.monitoring_interval
+  monitoring_role_arn                 = local.monitoring_role_arn
+  multi_az                            = var.multi_az
+  name                                = var.database_name
+  parameter_group_name                = local.parameter_group_name
+  password                            = var.password
+  performance_insights_enabled        = var.performance_insights_enabled
+  port                                = var.port
+  skip_final_snapshot                 = var.skip_final_snapshot
+  storage_encrypted                   = var.storage_encrypted
+  storage_type                        = var.storage_type
+  tags                                = local.db_tags
+  username                            = var.username
+  vpc_security_group_ids              = [aws_security_group.this.id]
+}
---
+++ modules/rds-postgres/outputs.tf
@@ -0,0 +1,25 @@
+output "address" {
+  description = "RDS database address"
+  value       = aws_db_instance.this.address
+}
+
+output "instance_connection_info" {
+  description = "Object containing connection info"
+  value = {
+    address  = aws_db_instance.this.address
+    endpoint = aws_db_instance.this.endpoint
+    id       = aws_db_instance.this.id
+    port     = aws_db_instance.this.port
+    username = aws_db_instance.this.username
+  }
+}
+
+output "instance_id" {
+  description = "Instance ID of RDS DB"
+  value       = aws_db_instance.this.id
+}
+
+output "username" {
+  description = "The master username for the database."
+  value       = var.username
+}
